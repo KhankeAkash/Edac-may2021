bab8562c9b1b3b9840d9ad054a6d18c04ef7430e
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 19:51:13 +0530

    Add files via upload

---
+++ assigement2/StarPattern4.java
@@ -0,0 +1,26 @@
+public class StarPattern4 
+{
+	public static void main(String args[])
+      {
+		
+		
+		for(int i=1;i<=5;i++) 
+                {
+			
+			for(int k=i;k>1;k--) 
+                         {
+			System.out.print(" ");
+			}
+			
+			for(int j=5;j>=i;j--)
+                        {
+				
+			System.out.print("*");
+				
+			}
+			
+			System.out.println();
+		}
+		
+	}
+}
