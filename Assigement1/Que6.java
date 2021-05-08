399f27d26008a54e4c9c6192340cf33e65981a1a
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 09:21:41 +0530

    Add files via upload

---
+++ Assigement1/Que6.java
@@ -0,0 +1,22 @@
+import java.util.*;
+  class Que6
+{
+public static void main(String args[])
+ {
+int i=125; int j=24;
+ 
+int k=i+j;
+int v=i-j;
+int a=i*j;
+int b=i/j;
+int s=125%24;
+
+System.out.println("k=i+j="+k);
+System.out.println("v=i-j="+v);
+System.out.println("a=i*j="+a);
+System.out.println("b=i/j="+b);
+System.out.println("s=i%j="+s);
+
+}
+
+}
