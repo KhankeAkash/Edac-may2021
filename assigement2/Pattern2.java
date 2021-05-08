af71da22f6293c0c7832c1b9d487a8cbbe3fb898
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 11:23:12 +0530

    Add files via upload

---
+++ assigement2/Pattern2.java
@@ -0,0 +1,26 @@
+class Pattern2
+{
+public static void main (String args[])
+
+ {
+    int words=64 ;
+
+     for(int i=1;i<=5;i++)
+   { 
+       for(int j=0;j<=i;j++)
+        {
+     
+        System.out.print ((char)(words+j)+" ");
+      
+         } 
+         System.out.println();
+
+
+   }
+
+
+ }
+
+
+}
+
