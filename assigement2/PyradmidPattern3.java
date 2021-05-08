e9bda80085b29e5d06d223fde3417ced1ac3416a
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 20:00:16 +0530

    Add files via upload

---
+++ assigement2/PyradmidPattern3.java
@@ -0,0 +1,19 @@
+public class PyradmidPattern3 {
+	public static void main(String args[]){
+		
+	java	for(int i=1;i<=9;i++){
+			
+			for(int k=8;k>=i;k--){
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++){
+				
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
