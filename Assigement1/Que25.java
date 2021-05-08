3ada2fe2d9b4c3f52711c157aea42b900a30b820
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 10:01:29 +0530

    Add files via upload

---
+++ Assigement1/Que25.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Que25 {
+	public static void main(String args[]){
+		//take input
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Octal Number: ");
+		int a = sc.nextInt();
+		
+		sc.close();
+		//process
+		
+		// 1. Binary to Decimal
+		int i = 0;
+		int d =0;
+		while(a!=0){
+			int k = a%10;
+			double power = Math.pow(8,i);
+			d = d + (k* (int)power);
+			a= a/10;
+			i++;
+		}
+		
+		
+		System.out.println(d);
+		
+		
+	}
+}
