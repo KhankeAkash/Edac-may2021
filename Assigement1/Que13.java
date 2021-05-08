c3c8ccedbcb6b9991a5b20546bc41365a387e634
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 09:33:47 +0530

    Add files via upload

---
+++ Assigement1/Que13.java
@@ -0,0 +1,10 @@
+class Que13
+{
+public static void main(String args[])
+{
+ double w=5.6; double h=8.5;int i=2;
+ System.out.println("Area is 5.6*8.5="+w*h);
+ System.out.println("perimter  is 2*(5.6+8.5)"=+i*(w+h)); 
+}
+
+}
