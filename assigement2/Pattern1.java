3f2d5a92737b2896e971c89012da77716343d17a
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 11:22:33 +0530

    Add files via upload

---
+++ assigement2/Pattern1.java
@@ -0,0 +1,26 @@
+class Pattern1
+{
+public static void main (String args[])
+
+ {
+    int r=1;
+
+     for(int i=1;i<=5;i++)
+   { 
+       for(int j=0;j<i;j++)
+        {
+     
+        System.out.print(r+j);
+      
+         } 
+         System.out.println();
+
+
+   }
+
+
+ }
+
+
+}
+
