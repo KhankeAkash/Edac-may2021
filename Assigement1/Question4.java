0e4bfb50e4d80efb2a4204e2e06800a574782ca6
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 09:19:15 +0530

    Add files via upload

---
+++ Assigement1/Question4.java
@@ -0,0 +1,28 @@
+class Question4
+{
+public static void main(String args[])
+{
+ int i;int j;int k;int g;
+
+  i=-5+8*6;
+System.out.print("-5+8*6=");
+System.out.println(i);
+System.out.println();
+
+  j=(55+9)%9;
+System.out.print("(55+9)%9=");
+System.out.print(j);
+System.out.println();
+
+  k=20+-3*5/8;
+System.out.print("20+3*5/8=");
+System.out.print(k);
+System.out.println();
+
+  g=5+15/3*2-8%3;
+System.out.print("5+15/3*2-8%3=");
+System.out.print(g);
+
+}
+
+}
