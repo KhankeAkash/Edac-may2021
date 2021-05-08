0a845c2c2af272e5989c0f34c72e1ee34566d51c
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 09:32:40 +0530

    Add files via upload

---
+++ Assigement1/Average.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Average
+{
+public static void main (String args[])
+{
+Scanner sc=new Scanner(System.in);
+  System.out.println("enter 3 no:");
+ int i=sc.nextInt();
+ int j=sc.nextInt();
+ int k=sc.nextInt();
+ int A=i+j+k;
+ 
+  System.out.println(" average:"+A/3 );
+}
+
+}
