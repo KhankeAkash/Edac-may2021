da47d11669d1d1f2d1974263217446b6ed0724f3
KhankeAkash <83824515+KhankeAkash@users.noreply.github.com>
2021-05-08 09:20:08 +0530

    Add files via upload

---
+++ Assigement1/Multi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Multi
+{ 
+ public static void main(String args[])
+{
+ Scanner sc= new Scanner(System.in);
+System.out.println("enter first no :");
+ int i=sc.nextInt();
+System.out.println("enter second no:");
+int j=sc.nextInt();
+int k=i*j;
+System.out.print("multiplication: ");
+System.out.print(k=i*j);
+}
+}
